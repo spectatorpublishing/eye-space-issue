0b3fb5d36226785d80a1589c895db42e0d8dd7bf
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-08-28 18:48:16 -0400

    Added data files

---
+++ src/data/sections.js
@@ -0,0 +1,23 @@
+export const sections = [
+    {
+        title: "Home",
+        url: "/",
+        exact: true,
+    },
+    {
+        title: "Lenape Land",
+        url: "/lenape-land",
+    },
+    {
+        title: "Columbia University",
+        url: "/columbia",
+    },
+    {
+        title: "Off Campus",
+        url: "/off-campus",
+    },
+    {
+        title: "Present Day",
+        url: "/present-day",
+    }
+];
