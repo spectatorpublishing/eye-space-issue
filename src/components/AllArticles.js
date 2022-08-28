93a256bc4aa828501c3fe4096d18934e50676043
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-08-28 18:48:31 -0400

    Create AllArticles.js

---
+++ src/components/AllArticles.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import { device } from '../device';
+import theme from '../theme';
+
+const Container = styled.div`
+    @media only screen and (max-width: 768px){
+
+    }
+`;
+
+const AllArticles = ({ }) => {
+    return (
+        <Container>
+            Articles
+        </Container>
+    )
+};
+
+export default AllArticles;
