cfb5034bf984cce9f15fae4e306883d848e1b4b6
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-08-28 18:48:38 -0400

    Create MainTitle.js

---
+++ src/components/MainTitle.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import { device } from '../device';
+import theme from '../theme';
+
+const Container = styled.div`
+    @media only screen and (max-width: 768px){
+
+    }
+`;
+
+const MainTitle = ({ }) => {
+    return (
+        <Container>
+            MainTitle
+        </Container>
+    )
+};
+
+export default MainTitle;
