e838214c2c3b3dd80c21c640edd536972ca81189
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-08-28 18:48:33 -0400

    Create Credits.js

---
+++ src/components/Credits.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import { device } from '../device';
+import theme from '../theme';
+
+const Container = styled.div`
+    @media only screen and (max-width: 768px){
+
+    }
+`;
+
+const Credits = ({ }) => {
+    return (
+        <Container>
+            Credits
+        </Container>
+    )
+};
+
+export default Credits;
