a3c6449fef8fa790c230b4aea34512b2c8503415
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-08-28 18:48:36 -0400

    Create LetterEditor.js

---
+++ src/components/LetterEditor.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import { device } from '../device';
+import theme from '../theme';
+
+const Container = styled.div`
+    @media only screen and (max-width: 768px){
+
+    }
+`;
+
+const LetterEditor = ({ }) => {
+    return (
+        <Container>
+            LetterEditor
+        </Container>
+    )
+};
+
+export default LetterEditor;
