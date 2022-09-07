455007bb5cc4ceee830873b32c93c04fffe44f32
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-09-06 23:04:57 -0400

    Update articles.js

--- src/data/articles.js
+++ src/data/articles.js
@@ -31,24 +31,24 @@ export const morningside_heights_articles = [
     "description": "She was expelled from Barnard in 1968 for living with her boyfriend. Read Grace LeClair's story: one that spurred the rapid evolution of Columbia’s residential spaces in the late 20th century."
   },
   {
-    "article_link": "https://www.columbiaspectator.com/eye/",
+    "article_link": "https://www.columbiaspectator.com/the-eye/2022/09/06/overruled-the-gay-lounge/",
     "article_title": "Overruled: The Gay Lounge",
     "article_authors": "Jonathan Truong",
-    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JY4EQCPCIZA4JD3ASOZYSPQEA4.gif",
+    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/BONTFFLVCZFOFHDWKXQRYKW3OM.jpeg",
     "description": "Once a storage closet in the Furnald Hall basement, Columbia’s Stephen Donaldson Lounge went on to become the first LGBTQ student lounge in the nation."
   },
   {
-    "article_link": "https://www.columbiaspectator.com/eye/",
-    "article_title": "The Lerner Story: Where do the Ramps Lead?",
+    "article_link": "https://www.columbiaspectator.com/the-eye/2022/09/06/the-lerner-story/",
+    "article_title": "The Lerner Story",
     "article_authors": "Caelan Bailey",
-    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JY4EQCPCIZA4JD3ASOZYSPQEA4.gif",
+    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/O3FMPESPDZB6BMXT5B622RF7M4.png",
     "description": "Lerner Hall was “meant to serve as a hi-tech version of Low Library steps on a Spring day.” Beyond Lerner’s ramps, the history of Columbia's student centers tell a story of space, competition, and disconnect."
   },
   {
-    "article_link": "https://www.columbiaspectator.com/eye/",
+    "article_link": "https://www.columbiaspectator.com/the-eye/2022/09/06/tunnel-vision/",
     "article_title": "Tunnel Vision",
     "article_authors": "Isabella Aldrete",
-    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JY4EQCPCIZA4JD3ASOZYSPQEA4.gif",
+    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/M6EVA4M3YNHBBOHU7HN462W7D4.jpg",
     "description": "Ghosts, asylums, secret gatherings: the tunnels underneath Columbia University seem ripped out of the pages of a gothic novel. But they’re real and full of a deep, complex history—older than campus itself."
   }
 ];
@@ -62,17 +62,17 @@ export const present_day_articles = [
     "description": "Six years ago, Columbia acknowledged the University’s role in displacing the Lenape for the first time. Searching for the specifics of their displacement in institutional documentation reveals a history of erasure, slavery, and neglect."
   },
   {
-    "article_link": "https://www.columbiaspectator.com/eye/",
+    "article_link": "https://www.columbiaspectator.com/the-eye/2022/09/06/whats-in-a-name-the-controversies-of-butler-library/",
     "article_title": "What’s in a name? The Controversies of Butler Library",
     "article_authors": "Lilienne Shore Kilgore-Brown",
-    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JY4EQCPCIZA4JD3ASOZYSPQEA4.gif",
+    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/7XQ4QCNJ4JH3HC4WULIPOT44FE.png",
     "description": "Butler Library's contemporary controversies have centered on the mostly white and male names on the edifice—but there’s another, darker side to Butler’s history and the man that built it."
   },
   {
-    "article_link": "https://www.columbiaspectator.com/eye/",
+    "article_link": "https://www.columbiaspectator.com/the-eye/2022/09/06/morningside-park-and-the-people-who-love-it/",
     "article_title": "Morningside Park and the People Who Love It",
     "article_authors": "Isabella Noonen",
-    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JY4EQCPCIZA4JD3ASOZYSPQEA4.gif",
+    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/7XQ4QCNJ4JH3HC4WULIPOT44FE.png",
     "description": "Columbia and Morningside Park have been neighbors for more than a century. It’s never been the easiest relationship."
   },
   {
@@ -83,24 +83,24 @@ export const present_day_articles = [
     "description": "She was expelled from Barnard in 1968 for living with her boyfriend. Read Grace LeClair's story: one that spurred the rapid evolution of Columbia’s residential spaces in the late 20th century."
   },
   {
-    "article_link": "https://www.columbiaspectator.com/eye/",
+    "article_link": "https://www.columbiaspectator.com/the-eye/2022/09/06/overruled-the-gay-lounge/",
     "article_title": "Overruled: The Gay Lounge",
     "article_authors": "Jonathan Truong",
-    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JY4EQCPCIZA4JD3ASOZYSPQEA4.gif",
+    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/BONTFFLVCZFOFHDWKXQRYKW3OM.jpeg",
     "description": "Once a storage closet in the Furnald Hall basement, Columbia’s Stephen Donaldson Lounge went on to become the first LGBTQ student lounge in the nation."
   },
   {
-    "article_link": "https://www.columbiaspectator.com/eye/",
-    "article_title": "The Lerner Story: Where do the Ramps Lead?",
+    "article_link": "https://www.columbiaspectator.com/the-eye/2022/09/06/the-lerner-story/",
+    "article_title": "The Lerner Story",
     "article_authors": "Caelan Bailey",
-    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JY4EQCPCIZA4JD3ASOZYSPQEA4.gif",
+    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/O3FMPESPDZB6BMXT5B622RF7M4.png",
     "description": "Lerner Hall was “meant to serve as a hi-tech version of Low Library steps on a Spring day.” Beyond Lerner’s ramps, the history of Columbia's student centers tell a story of space, competition, and disconnect."
   },
   {
-    "article_link": "https://www.columbiaspectator.com/eye/",
+    "article_link": "https://www.columbiaspectator.com/the-eye/2022/09/06/tunnel-vision/",
     "article_title": "Tunnel Vision",
     "article_authors": "Isabella Aldrete",
-    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JY4EQCPCIZA4JD3ASOZYSPQEA4.gif",
+    "image_url": "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/M6EVA4M3YNHBBOHU7HN462W7D4.jpg",
     "description": "Ghosts, asylums, secret gatherings: the tunnels underneath Columbia University seem ripped out of the pages of a gothic novel. But they’re real and full of a deep, complex history—older than campus itself."
   }
 ];
