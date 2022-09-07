225b8535d28bd7969fe3a23d09dccdc8a0a6ef16
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-09-07 19:27:04 -0400

    Update Illos.js

--- src/containers/Illos.js
+++ src/containers/Illos.js
@@ -11,6 +11,10 @@ const Wrapper = styled.main`
 const IlloWrapper = styled.div`
     width: 50%;
     margin: 1rem auto 3rem auto;
+
+    @media only screen and (max-width: 768px){
+        width: 80%;
+    }
 `
 
 const Location = styled.h2`
