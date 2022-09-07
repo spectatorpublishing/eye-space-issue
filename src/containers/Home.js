1aeffcef3c53c0fe438e95a6258780f301c143c8
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-09-06 22:29:27 -0400

    added font awesome

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -17,8 +17,6 @@ const Wrapper = styled.main`
 `;
 
 const HomeContainer = styled.div`
-    width: 100%;
-    height: 100%;
     display: flex;
     flex-direction: column;
     background-color: ${theme.colors.tan};
